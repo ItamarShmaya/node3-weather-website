cb7e8cbfb7e7a57b9cf0b272ce7c2ed77976d45d
Itamar <itamarshmaya@gmail.com>
2022-06-15 13:01:21 +0300

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
@@ -44,6 +45,6 @@ app.get("/weather", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log("server is up on port " + port);
 });
